234db579c45691ec683c64b75fa2b2aa63dd9f2a
hillarydalie <hidalie@gmail.com>
2019-09-16 02:41:52 +0300

    Add test comment after the test failed

---
+++ tests/test_comment.py
@@ -0,0 +1,21 @@
+import unittest
+from .app import models
+
+class CommentsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Article class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_comment = new_comment(
+            "1",
+            "hillarydalie", 
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse", 
+            "24th Sept 2019"
+            )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment,Comment))
