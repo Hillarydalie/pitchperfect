afd1b35a3661c5c75262bfa2593a1499b7480fba
hillarydalie <hidalie@gmail.com>
2019-09-16 02:42:50 +0300

    Add model class for posts and blog

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app import create_app,db
-from app.models import User
+from app.models import User,Post,Comment
+
 app=create_app()
 
 manager =  Manager(app)
@@ -14,7 +15,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def add_shell_context():
-    return {"db":db,"User":User}
+    return {"db":db,"User":User,"Post":Post}
 
 if __name__=="__main__":
     manager.run()
