2cc7f2c1e664e3b2f790af02ba58589493598645
hillarydalie <hidalie@gmail.com>
2019-09-16 02:44:13 +0300

    Add route for about and pitch

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,19 @@ from flask import render_template
 from flask_login import login_required
 from . import main
 
-
 @main.route('/')
 @login_required
 def index():
+    return render_template('index.html')
+
+
+@main.route('/about')
+@login_required
+def about():
+    return render_template('about.html')
+
+@main.route('/pitches')
+@login_required
+def pitches():
     return render_template('index.html')
     
