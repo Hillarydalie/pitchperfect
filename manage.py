7c9ac440e61762c189e6e360c6a944d605a5cca8
hillarydalie <hidalie@gmail.com>
2019-09-18 11:11:04 +0300

    Initiate db in app

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app import create_app,db
-from app.models import User,Post,Comment
+from app.models import User
 
-app=create_app()
+app = create_app()
+# app = create_app('production')
 
 manager =  Manager(app)
 # Initiate the migrate we import
@@ -13,9 +14,10 @@ manager.add_command('runserver',Server(use_debugger=True))
 manager.add_command('db',MigrateCommand)
 
 
+
 @manager.shell
 def add_shell_context():
-    return {"db":db,"User":User,"Post":Post}
+    return {"db":db,"User":User}
 
 if __name__=="__main__":
     manager.run()
