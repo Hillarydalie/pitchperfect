fea5a994d8e4e9dc28b18574a304873daf7e3933
hillarydalie <hidalie@gmail.com>
2019-09-20 09:35:11 +0300

    Config setup

--- app/__init__.py
+++ app/__init__.py
@@ -17,12 +17,12 @@ login_manager.login_view = "auth.login"
 # Change how secret kkey is generated
 login_manager.session_protection = "strong"
 
-def create_app():
-    app.config.from_object(Config)
+def create_app(config_name):
+    app.config.from_object(configurations[config_name])
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint    
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(auth_blueprint)\
+    app.register_blueprint(auth_blueprint)
 
     bootstrap.init_app(app)
     db.init_app(app)
--- config.py
+++ config.py
@@ -6,8 +6,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hillarydalie:password@localhost:5432/pitchperfect'
 
-class ProdConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
  # email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -15,3 +13,13 @@ class ProdConfig:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class DevConfig(Config):
+    
+        
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+configurations = {"production" = ProdConfig, "development" = DevConfig}
